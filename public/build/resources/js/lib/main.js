f04452aa9cd9f629bf8cc11a1d541f1f0288b01b
hoolookook <hoolookook@gmail.com>
2022-01-26 16:25:52 +0900

    webroot test

--- public/build/resources/js/lib/main.js
+++ public/build/resources/js/lib/main.js
@@ -19,11 +19,11 @@ $(document).ready(function () {
 
     $(".logo img").attr(
       "src",
-      "@@webRoot/resources/images/icons/header-ci-black.png"
+      "../../resources/images/icons/header-ci-black.png"
     );
     $(".navWrap .nav .login").css({
       "background-image":
-        "url(@@webRoot/resources/images/icons/nav-login-icon-black.png)",
+        "url(../../resources/images/icons/nav-login-icon-black.png)",
     });
     $(".navWrap .nav a").css({
       color: "black",
@@ -35,11 +35,11 @@ $(document).ready(function () {
     });
     $(".logo img").attr(
       "src",
-      "@@webRoot/resources/images/icons/header-ci-white.png"
+      "../../resources/images/icons/header-ci-white.png"
     );
     $(".navWrap .nav .login").css({
       "background-image":
-        "url(@@webRoot/resources/images/icons/nav-login-icon-white.png)",
+        "url(../../resources/images/icons/nav-login-icon-white.png)",
     });
     $(".navWrap .nav a").css({
       color: "white",
