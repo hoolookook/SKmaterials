602aad7917cf9bce86ad1f42f5dd70b7c320ece4
hoolookook <hoolookook@gmail.com>
2022-02-10 00:53:29 +0900

    media query

--- public/src/resources/js/lib/main.js
+++ public/src/resources/js/lib/main.js
@@ -83,21 +83,36 @@ $(document).ready(function () {
     var p = matchMedia("screen and (max-width: 991px)");
     var q = matchMedia("screen and (max-width: 767px)");
     var r = matchMedia("screen and (max-width: 575px)");
+    var s = matchMedia("screen and (max-width: 393px)");
+    var t = matchMedia("screen and (max-width: 393px)");
     // if
     if (matchMedia(m.media).matches) {
       barRaise(true, "190", "120", "380", "60");
+      $(".progressBar").css({ width: "60px" });
     }
     if (matchMedia(o.media).matches) {
       barRaise(true, "171", "108", "342", "54");
+      $(".progressBar").css({ width: "54px" });
     }
     if (matchMedia(p.media).matches) {
       barRaise(true, "152", "96", "304", "48");
+      $(".progressBar").css({ width: "48px" });
     }
     if (matchMedia(q.media).matches) {
       barRaise(true, "133", "84", "266", "42");
+      $(".progressBar").css({ width: "42px" });
     }
     if (matchMedia(r.media).matches) {
       barRaise(true, "114", "72", "228", "36");
+      $(".progressBar").css({ width: "60px" });
+    }
+    if (matchMedia(s.media).matches) {
+      barRaise(true, "94", "52", "188", "30");
+      $(".progressBar").css({ width: "60px" });
+    }
+    if (matchMedia(t.media).matches) {
+      barRaise(true, "74", "32", "128", "24");
+      $(".progressBar").css({ width: "60px" });
     } else {
     }
   });
@@ -244,7 +259,7 @@ $(document).ready(function () {
     fade: true,
     cssEase: "linear",
     arrow: false,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 15000,
   });
 
